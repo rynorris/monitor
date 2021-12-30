3890eb9820f62b5d9b3a2b1450024a41705a64d0
Ryan Norris <rnorris@palantir.com>
2021-12-30 20:26:05 +0900

    Add crypto wrapper functions

---
+++ frontend/src/crypto.ts
@@ -0,0 +1,65 @@
+
+export async function generateEncryptionKey(): Promise<CryptoKey> {
+	return window.crypto.subtle.generateKey(
+		{
+			name: "AES-GCM",
+			length: 256,
+		},
+		true,
+		["encrypt", "decrypt"],
+	);
+}
+
+export async function encrypt(key: CryptoKey, data: BufferSource, iv: BufferSource): Promise<ArrayBuffer> {
+	return window.crypto.subtle.encrypt(
+		{
+			name: "AES-GCM",
+			iv,
+		},
+		key,
+		data,
+	);
+}
+
+export async function decrypt(key: CryptoKey, data: BufferSource, iv: BufferSource): Promise<ArrayBuffer> {
+	return window.crypto.subtle.decrypt(
+		{
+			name: "AES-GCM",
+			iv,
+		},
+		key,
+		data,
+	);
+}
+
+export async function generateKeyPair(): Promise<CryptoKeyPair> {
+	return window.crypto.subtle.generateKey(
+		{
+			name: "ECDSA",
+			namedCurve: "P-256",
+		},
+		true,
+		["sign", "verify"],
+	);
+}
+
+export async function sign(key: CryptoKey, data: BufferSource): Promise<ArrayBuffer> {
+	return window.crypto.subtle.sign(
+		{
+			name: "ECDSA",
+		},
+		key,
+		data,
+	);
+}
+
+export async function verify(key: CryptoKey, signature: BufferSource, data: BufferSource): Promise<boolean> {
+	return window.crypto.subtle.verify(
+		{
+			name: "ECDSA",
+		},
+		key,
+		signature,
+		data,
+	);
+}
