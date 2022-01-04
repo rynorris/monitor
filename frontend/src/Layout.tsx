3d31b2d1c27fc76f17a8df4f84f9cf7463c4ead5
Ryan Norris <rnorris@palantir.com>
2022-01-04 13:14:23 +0900

    Add feature compatibility barrier

--- frontend/src/Layout.tsx
+++ frontend/src/Layout.tsx
@@ -3,13 +3,16 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import { AppHeader } from "./components/AppHeader";
 import { Navbar } from "./components/Navbar";
+import { CompatibilityBarrier } from "./components/CompatibilityBarrier";
 
 export const Layout: React.FC = () => {
     return (
         <Grid height="100%" gridTemplateRows="max-content 1fr max-content">
             <AppHeader />
             <Box flexGrow={1} overflow="hidden">
-                <Outlet />
+                <CompatibilityBarrier>
+                    <Outlet />
+                </CompatibilityBarrier>
             </Box>
             <Navbar />
         </Grid>
---
+++ frontend/src/components/CompatibilityBarrier.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { Alert, AlertIcon, Center, Stack, Text } from "@chakra-ui/react";
+
+import { VIDEO_CODEC } from "../media";
+
+interface CompatibilityCheck {
+    name: string;
+    isSupported: () => boolean;
+}
+
+const CHECKS: CompatibilityCheck[] = [
+    {
+        name: "Media Source Extensions",
+        isSupported: () => typeof MediaSource !== undefined,
+    },
+    {
+        name: "Webm recording",
+        isSupported: () => MediaRecorder.isTypeSupported(VIDEO_CODEC),
+    },
+    {
+        name: "Webm playback",
+        isSupported: () => MediaSource.isTypeSupported(VIDEO_CODEC),
+    },
+];
+
+export const CompatibilityBarrier: React.FC = ({ children }) => {
+    const allSupported = CHECKS.every(check => check.isSupported());
+
+    if (allSupported) {
+        return <>{children}</>;
+    } else {
+        const alerts = CHECKS.map(check => (
+            <Alert status={check.isSupported() ? "success" : "error"}>
+                <AlertIcon />
+                {check.name}
+            </Alert>
+        ));
+
+        return (
+            <Center width="100%" height="100%" padding={4}>
+                <Stack spacing={3}>
+                    <Text>Your browser does not support all the features required for this app.</Text>
+                    {alerts}
+                </Stack>
+            </Center>
+        );
+    }
+}
