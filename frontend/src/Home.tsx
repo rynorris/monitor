79d3fa974fb86bb94e90a66145fd597b275bce0d
Ryan Norris <rnorris@palantir.com>
2022-01-07 20:16:25 +0900

    Improve home page copy

--- frontend/src/Home.tsx
+++ frontend/src/Home.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Heading, Text } from "@chakra-ui/react";
+import { Button, Container, Heading, Text } from "@chakra-ui/react";
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
@@ -6,22 +6,23 @@ export const Home: React.FC = () => {
     const navigate = useNavigate();
 
     return (
-        <Box padding={2}>
+        <Container height="100%" width="100%" padding={4} overflow="auto">
             <Text mb={4}>
                 Turn an old phone into a secure, internet-connected baby monitor in
                 one click!
             </Text>
+            <Heading size="lg" mb={2}>Easy</Heading>
+            <Text mb={4}>
+                Set up a stream with a single click, and easily share stream details with other devices via a QR code.
+            </Text>
             <Heading size="lg" mb={2}>Private</Heading>
             <Text mb={4}>
-                Video streams are keyed by an opaque ID, and not discoverable via the server.
+                There is no way to search for streams.  They are <strong>only</strong> discoverable via scanning your QR code.
             </Text>
             <Heading size="lg" mb={2}>Secure</Heading>
             <Text mb={4}>
                 All video data is encrypted using industry-standard strong encryption methods, and the keys are <strong>not</strong> shared with the server.
-            </Text>
-            <Heading size="lg" mb={2}>Easy</Heading>
-            <Text mb={4}>
-                Set up a stream with a single click, and easily share stream details with other devices via a QR code.
+                The <strong>only</strong> way to view the content is to receive the encryption keys by scanning the QR code.
             </Text>
             <Button
                 onClick={() => navigate("/broadcast")}
@@ -33,7 +34,6 @@ export const Home: React.FC = () => {
             >
                 Start Broadcasting
             </Button>
-
-        </Box>
+        </Container>
     );
 }
