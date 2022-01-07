6ae344c5134577280f9d681b811b6919a3ed18e4
Ryan Norris <rnorris@palantir.com>
2022-01-07 20:02:54 +0900

    Add copy to home page

--- frontend/src/Home.tsx
+++ frontend/src/Home.tsx
@@ -1,11 +1,39 @@
-import { Box, Text } from "@chakra-ui/react";
+import { Box, Button, Heading, Text } from "@chakra-ui/react";
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
-export const Home: React.FC = () => (
-    <Box padding={2}>
-        <Text>
-            Turn an old phone into a secure, internet-connected baby monitor in
-            one click!
-        </Text>
-    </Box>
-);
+export const Home: React.FC = () => {
+    const navigate = useNavigate();
+
+    return (
+        <Box padding={2}>
+            <Text mb={4}>
+                Turn an old phone into a secure, internet-connected baby monitor in
+                one click!
+            </Text>
+            <Heading size="lg" mb={2}>Private</Heading>
+            <Text mb={4}>
+                Video streams are keyed by an opaque ID, and not discoverable via the server.
+            </Text>
+            <Heading size="lg" mb={2}>Secure</Heading>
+            <Text mb={4}>
+                All video data is encrypted using industry-standard strong encryption methods, and the keys are <strong>not</strong> shared with the server.
+            </Text>
+            <Heading size="lg" mb={2}>Easy</Heading>
+            <Text mb={4}>
+                Set up a stream with a single click, and easily share stream details with other devices via a QR code.
+            </Text>
+            <Button
+                onClick={() => navigate("/broadcast")}
+                flex="none"
+                colorScheme="green"
+                width="100%"
+                height="50px"
+                maxW="500px"
+            >
+                Start Broadcasting
+            </Button>
+
+        </Box>
+    );
+}
